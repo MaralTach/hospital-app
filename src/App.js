691bea7c8fb5c8d92118c478beaf549254eacb93
MaralTach <yoloten.maral@gmail.com>
2024-04-01 21:52:16 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import "./App.css";
-import { useState } from "react";
-import { appointmentData } from "./helpers/Data";
-import AddModal from "./components/AddModal";
-
 import Home from "./pages/Home";
-// import { doctorData } from "./helpers/Data";
-// import Doctors from "./components/Doctors";
 import "bootstrap/dist/css/bootstrap.min.css";
-import AppointmentList from "./components/AppointmenList";
 
 function App() {
-  const [appointments, setAppointments] = useState(appointmentData);
   return (
     <div className="App">
       <Home  />
-
     </div>
   );
 }
